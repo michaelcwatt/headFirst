f69693228eef5cf550bc04e1bf9e779dac486df1
michael watt <michaelcolinwatt@icloud.comgit config --global user.name michael>
2024-05-07 22:33:46 +0100

    1 - headfirst notes.

--- script2.js
+++ script2.js
@@ -1,4 +1,17 @@
-setTimeout (wakeUpUser, 5000);
-function wakeUpUser() {
-    alert("wake the fuck up bitch!");
-}
+// guessing what each line of code does//
+
+    //var price = 28.99 - it creates a variable called price and stores a number in it.
+    // var discount = 10 - creating another variable called discount and storing 10 in it.
+    // var total = price-(price*(discount/100)) -this takes discount devides by 100 times price by that and deducts price by that.
+    // it the total is over 25 run next line 
+    // free shipping() this calls free shipping function. 
+    // creates a cariable called count and stores 10 in it.
+    // conditional statment if count is more than 0 run the next line of code 
+    // calls the juggle function 
+    //removes one from count.
+    //creates variable called dog assigned some sort of holding thing for name and weight
+    //if dog weight is more than 30 then run next line 
+    //displays alert saying woof woof else run next line 
+    // alert on page saying woof woof.
+    //creates variable circleRadious and puts 20 in it 
+    //no idea about that one.
